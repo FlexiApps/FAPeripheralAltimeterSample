02dbd874319b7aafa92db3646e1d6efc55dddff6
christos christodoulou <xxristos@gmail.com>
2017-10-26 12:32:14 +0300

    initial commit

--- Pods/Target Support Files/Pods-iOSCollarSampleData/Pods-iOSCollarSampleData-dummy.m
+++ Pods/Target Support Files/Pods-iOSCollarSampleData/Pods-iOSCollarSampleData-dummy.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSCollarSampleData : NSObject
+@interface PodsDummy_Pods_FAPeripheralAltimeterSample : NSObject
 @end
-@implementation PodsDummy_Pods_iOSCollarSampleData
+@implementation PodsDummy_Pods_FAPeripheralAltimeterSample
 @end
